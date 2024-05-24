28f793b7662776953ce210edde0ab80b760c8293
elfeny7 <ikmalfaris50@gmail.com>
2024-05-24 07:35:48 +0700

    add: route api door and user

--- app/Http/Controllers/Api/DoorController.php
+++ app/Http/Controllers/Api/DoorController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Door;
+use App\Http\Resources\DoorResource;
 
 class DoorController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get posts
+        $posts = Door::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new DoorResource(true, 'List Data Posts', $posts);
+    }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get posts
+        $posts = User::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new UserResource(true, 'List Data Posts', $posts);
+    }
 }
